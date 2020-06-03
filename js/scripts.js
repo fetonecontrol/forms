5e03ff713cfa64f1de67171b4ee2d480b6fdb9a4
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-03 10:26:21 -0700

    add javascript for form submission
    co-authored-by: Joseph Pearce

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,16 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val();
-    const animalInput= $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
+  $("#formLetter").submit(function(event) {
+    let name = $("name").val();
+    let message = $("message").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    $(".name").text(name);
+    $(".message").text(message);
 
-    $("#story").show();
+    $("#letter").show();
 
     event.preventDefault();
+
   });
+
+  
 });
